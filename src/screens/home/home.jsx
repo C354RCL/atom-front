c33a62c049958b03aef2c1b15a3537e2bd950ca7
Ange1DA <adeanda10@alumnos.uaq.mx>
2023-12-06 11:20:48 -0600

    Dinamic pages
    Ya no jala jajaja pero ahi vamos

--- src/screens/home/home.jsx
+++ src/screens/home/home.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 //import ReactDOM from "react-dom/client";
 import "./home.css";
 
@@ -7,37 +7,44 @@ import { Habit } from "../../components/habit/habit.jsx";
 
 export function Home() {
   // Obtenemos userName desde localStorage
-  const userName = localStorage.getItem('userName');
+  const userName = localStorage.getItem("userName");
   // Creamos una funcion que se ejecuta en cuanto se monta el componente
   useEffect(() => {
-    // Creamos una funcion asincrona 
-    const fetchData = async() => {
+    // Creamos una funcion asincrona
+    const fetchData = async () => {
       try {
         // Creamos la peticion a la API
-        const res = await fetch(`http://localhost:3000/main?userName=${userName}`, {
-        method : 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-        }
-        });
-    
+        const res = await fetch(
+          `http://localhost:3000/main?userName=${userName}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
         // Si la respuesta no es 'ok'
         if (!res.ok) {
-          throw new Error('Error en la solicitud');
+          throw new Error("Error en la solicitud");
         }
-        
+
         // Convertimos la respuesta en formato JSON
         const data = await res.json();
-        console.log(data);    
+        console.log(data);
       } catch (error) {
         console.error(error);
       }
-    }
+    };
     fetchData();
   });
 
   return (
     <>
+      {data.map((item, index) => (
+        <Habit key={index} data{item}></Habit>
+      ))}
+
       <h1>Tus habitos...</h1>
       <Habit />
       <Habit />
