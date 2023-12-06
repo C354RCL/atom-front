bbcd36654f44028a255fa11213ca684a2f394488
Ange1DA <adeanda10@alumnos.uaq.mx>
2023-12-06 10:45:06 -0600

    Acceso
    Ya jala el acceso con las credenciales, a ver si no lo desmadro intentando mandar una notificacion cuando no mete las credenciales correctas.

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import logo from './img/atomlogo.png';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import logo from "./img/atomlogo.png";
 
 export function Login() {
   // Inicializamos las variables como cadenas vacias
-  const [userName, setUserName] = useState('');
-  const [passwd, setPasswd] = useState('');
+  const [userName, setUserName] = useState("");
+  const [passwd, setPasswd] = useState("");
+  const navigate = useNavigate();
 
-  // Manejador de eventos de userName 
+  // Manejador de eventos de userName
   const handleUserName = (e) => {
     setUserName(e.target.value);
   };
@@ -22,44 +24,53 @@ export function Login() {
     e.preventDefault();
 
     try {
-      // Creamos la peticion a la API 
-      const res = await fetch('http://localhost:3000/signin', {
-        method: 'POST',
+      // Creamos la peticion a la API
+      const res = await fetch("http://localhost:3000/signin", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({ userName, passwd }),
       });
 
       // Guardamos userName en localStorage
-      localStorage.setItem('userName', userName);
+      localStorage.setItem("userName", userName);
       // La respuesta del servidor la convertimos a formato JSON
       const data = await res.json();
       // Guardamos el token en localStorage
-      localStorage.setItem('token', data.token);
+      localStorage.setItem("token", data.token);
       // Se imprime el resultado
       console.log(data);
+
+      //
+
+      if (data.code === 200) {
+        // Realiza la redirección a "/home"
+        navigate("/home");
+        console.log(data.code);
+      } else {
+      }
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
 
   return (
     <div className="login">
       <img src={logo} alt="logo" className="image" />
-      <input 
-      type="text" 
-      placeholder="Usuario" 
-      className="item" 
-      value={userName} 
-      onChange={handleUserName} 
+      <input
+        type="text"
+        placeholder="Usuario"
+        className="item"
+        value={userName}
+        onChange={handleUserName}
       />
-      <input 
-      type="password" 
-      placeholder="Contraseña" 
-      className="item" 
-      value={passwd} 
-      onChange={handlePasswd} 
+      <input
+        type="password"
+        placeholder="Contraseña"
+        className="item"
+        value={passwd}
+        onChange={handlePasswd}
       />
       <button type="submit" className="item" onClick={handleSubmit}>
         <Link to="/home" className="loginb">
